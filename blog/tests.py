a9c05a82d4bc151b4a1925cbe4f41a7c30377c75
peijipe <honda@k--s.com>
2018-09-15 16:18:43 +0900

    make_unittest

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_index_acccess(self):
+        res = self.c.get('/')
+        self.assertEqual(200, res.status_code)
+
+    def test_fail_acccess(self):
+        res = self.c.get('/unkwoun')
+        self.assertEqual(404, res.status_code)
+    
